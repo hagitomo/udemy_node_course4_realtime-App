af3e85cf068228a44164668ca4bc416919d94458
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-01-06 00:35:51 +0900

    add welcome Msg and welcome user

--- server/server.js
+++ server/server.js
@@ -17,6 +17,20 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('new user connectedn...')
 
+  // 全員へ送信されるメッセージ
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat',
+    createdAt: new Date().getTime()
+  })
+
+  // 新規参加者から、既存のメンバーに送信されるメッセージ
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('new message', message)
     io.emit('newMessage', {
@@ -24,6 +38,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
