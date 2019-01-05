2b485572db3c9f506b6464eb4ecee56facccc260
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-01-05 20:45:02 +0900

    socket io add

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 const app = express()
+const server = http.createServer(app)
+const io = socketIO(server) // socket io使用
+
 
 // 環境変数
 const port = process.env.PORT || 3000;
+
 // 静的ファイル利用
 const publicPath = path.join(__dirname, '../public')
 app.use(express.static(publicPath))
 
-app.listen(port, () =>{
-  console.log('Server is starting........')
+// socket io
+io.on('connection', (socket) => {
+  console.log('new user connectedn...')
+
+  socket.on('disconnect', () => {
+    console.log('user was disconnected...')
+  })
+})
+
+server.listen(port, () =>{
+  console.log('Server is starting//////')
 })
