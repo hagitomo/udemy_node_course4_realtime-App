c2dd6b5c1e0c8d25ff7d24a988da1fadbb72b180
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2019-01-11 23:03:22 +0900

    add mustache.js and fix template

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,17 @@ socket.on('disconnect', function() {
 })
 
 socket.on('newMessage', function(message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a')
+    var formattedTime = moment(message.createdAt).format('h:mm a')
 
-  var li = $('<li></li>')
-  li.text(`${message.from} ${formattedTime}: ${message.text}`)
-  $('#messages').append(li)
+    // mustache.js
+    var template = $('#message-template').html()
+    var html = Mustache.render(template, {
+      text: message.text,
+      from: message.from,
+      createdAt: formattedTime
+    })
+
+    $('#messages').append(html)
 })
 
 // formイベント
@@ -59,13 +65,14 @@ locationButton.on('click', function () {
 // クライアント側でのnewLocationMessageイベントに対応
 socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a')
-  var li = $('<li></li>')
-  var a = $('<a target="_blank">My current location</a>')
 
-  li.text(`${message.from} ${formattedTime}: `)
-  a.attr('href', message.url)
-  li.append(a)
+  var template = $('#location-message-template').html()
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  })
 
-  $('#messages').append(li)
+  $('#messages').append(html)
 })
 
